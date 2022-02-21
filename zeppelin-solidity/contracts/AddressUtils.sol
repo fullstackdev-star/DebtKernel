e47289af42fbe40d51b0b941e56b8f0200d05cf9
Jin Qian <jin_q@outlook.com>
2022-02-21 10:22:17 +0800

    Update

---
+++ zeppelin-solidity/contracts/AddressUtils.sol
@@ -0,0 +1,49 @@
+
+/**
+ * Utility library of inline functions on addresses
+ */
+ 
+library AddressUtils {
+
+  /**
+   * Returns whether there is code in the target address
+   * @dev This function will return false if invoked during the constructor of a contract,
+   *  as the code is not actually created until after the constructor finishes.
+   * @param addr address address to check
+   * @return whether there is code in the target address
+   */
+  function isContract(address addr) internal view returns (bool) {
+    uint256 size;
+    assembly { size := extcodesize(addr) }
+    return size > 0;
+  }
+
+}
+
+/**
+ * @title ERC721 token receiver interface
+ * @dev Interface for any contract that wants to support safeTransfers
+ *  from ERC721 asset contracts.
+ */
+contract ERC721Receiver {
+  /**
+   * @dev Magic value to be returned upon successful reception of an NFT
+   *  Equals to `bytes4(keccak256("onERC721Received(address,uint256,bytes)"))`,
+   *  which can be also obtained as `ERC721Receiver(0).onERC721Received.selector`
+   */
+  bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba; 
+
+  /**
+   * @notice Handle the receipt of an NFT
+   * @dev The ERC721 smart contract calls this function on the recipient
+   *  after a `safetransfer`. This function MAY throw to revert and reject the
+   *  transfer. This function MUST use 50,000 gas or less. Return of other
+   *  than the magic value MUST result in the transaction being reverted.
+   *  Note: the contract address is always the message sender.
+   * @param _from The sending address 
+   * @param _tokenId The NFT identifier which is being transfered
+   * @param _data Additional data with no specified format
+   * @return `bytes4(keccak256("onERC721Received(address,uint256,bytes)"))`
+   */
+  function onERC721Received(address _from, uint256 _tokenId, bytes _data) public returns(bytes4);
+}
---
+++ zeppelin-solidity/contracts/token/ERC721/ERC721.sol
@@ -0,0 +1,50 @@
+
+/**
+ * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
+ * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+contract ERC721Enumerable is ERC721Basic {
+  function totalSupply() public view returns (uint256);
+  function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256 _tokenId);
+  function tokenByIndex(uint256 _index) public view returns (uint256);
+}
+
+/**
+ * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
+ * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+contract ERC721Enumerable is ERC721Basic {
+  function totalSupply() public view returns (uint256);
+  function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256 _tokenId);
+  function tokenByIndex(uint256 _index) public view returns (uint256);
+}
+
+/**
+ * @title ERC-721 Non-Fungible Token Standard, optional metadata extension
+ * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+contract ERC721Metadata is ERC721Basic {
+  function name() public view returns (string _name);
+  function symbol() public view returns (string _symbol);
+  function tokenURI(uint256 _tokenId) public view returns (string);
+}
+
+/**
+ * @title ERC-721 Non-Fungible Token Standard, full implementation interface
+ * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+contract ERC721 is ERC721Basic, ERC721Enumerable, ERC721Metadata {
+}
+
+// File: zeppelin-solidity/contracts/token/ERC721/DeprecatedERC721.sol
+
+/**
+ * @title ERC-721 methods shipped in OpenZeppelin v1.7.0, removed in the latest version of the standard
+ * @dev Only use this interface for compatibility with previously deployed contracts
+ * @dev Use ERC721 for interacting with new contracts which are standard-compliant
+ */
+contract DeprecatedERC721 is ERC721 {
+  function takeOwnership(uint256 _tokenId) public;
+  function transfer(address _to, uint256 _tokenId) public;
+  function tokensOf(address _owner) public view returns (uint256[]);
+}
